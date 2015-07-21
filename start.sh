df42d4a55e6dd23e57551ed1510f713f6cf677c7
Ian Atha <ian@atha.io>
2015-07-21 12:18:01 +0000

    gce, no gpu

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@
 # the inputs/ directory and spew out results into outputs/
 
 PORT=80
-DOCKER_NVIDIA_DEVICES="--device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm"
+# DOCKER_NVIDIA_DEVICES="--device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm"
 docker run  --restart=always --name deepdream-files -v `pwd`/deepdream:/usr/share/nginx/html:ro -d -p ${PORT}:80 nginx
-docker run  --restart=always --name deepdream-compute $DOCKER_NVIDIA_DEVICES -v `pwd`/deepdream:/opt/deepdream -d tleyden5iwx/caffe-gpu-master /bin/bash -c "cd /opt/deepdream && ./process_images.sh"
+docker run  --restart=always --name deepdream-compute $DOCKER_NVIDIA_DEVICES -v `pwd`/deepdream:/opt/deepdream -d dream /bin/bash -c "cd /opt/deepdream && ./process_images.sh"

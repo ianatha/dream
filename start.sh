2749e4d26bd1ee56dca98d0a69fe778e0b3a57da
Ian W. Atha <ian@gumroad.com>
2015-07-21 03:37:58 -0700

    use nvidia gpu

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@
 # the inputs/ directory and spew out results into outputs/
 
 PORT=80
+DOCKER_NVIDIA_DEVICES="--device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm"
 docker run --name deepdream-files -v `pwd`/deepdream:/usr/share/nginx/html:ro -d -p ${PORT}:80 nginx
-docker run --name deepdream-compute -v `pwd`/deepdream:/opt/deepdream -d visionai/clouddream /bin/bash -c "cd /opt/deepdream && ./process_images.sh"
+docker run --name deepdream-compute $DOCKER_NVIDIA_DEVICES -v `pwd`/deepdream:/opt/deepdream -d tleyden5iwx/caffe-gpu-master /bin/bash -c "cd /opt/deepdream && ./process_images.sh"

f7c66acc517177625c638976b67fb9a85e864a3b
Ian Atha <ian@atha.io>
2015-07-21 11:13:46 +0000

    use gpu

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@
 
 PORT=80
 DOCKER_NVIDIA_DEVICES="--device /dev/nvidia0:/dev/nvidia0 --device /dev/nvidiactl:/dev/nvidiactl --device /dev/nvidia-uvm:/dev/nvidia-uvm"
-docker run --name deepdream-files -v `pwd`/deepdream:/usr/share/nginx/html:ro -d -p ${PORT}:80 nginx
-docker run --name deepdream-compute $DOCKER_NVIDIA_DEVICES -v `pwd`/deepdream:/opt/deepdream -d tleyden5iwx/caffe-gpu-master /bin/bash -c "cd /opt/deepdream && ./process_images.sh"
+docker run  --restart=always --name deepdream-files -v `pwd`/deepdream:/usr/share/nginx/html:ro -d -p ${PORT}:80 nginx
+docker run  --restart=always --name deepdream-compute $DOCKER_NVIDIA_DEVICES -v `pwd`/deepdream:/opt/deepdream -d tleyden5iwx/caffe-gpu-master /bin/bash -c "cd /opt/deepdream && ./process_images.sh"
